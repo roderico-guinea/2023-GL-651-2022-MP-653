bae41b0852ebd082a0708622d808477bd228a5fd
Roderico Guinea <guinearoderico2@gmail.com>
2025-02-20 18:04:53 -0600

    Agregar archivos de proyecto.

---
+++ L0_NUMEROS_CARNETS/Models/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L0_NUMEROS_CARNETS.Models
+{
+    public class Usuarios   {
+
+        [Key]
+
+        public int usuarioId { get; set; }
+
+        public int rollId  { get; set; }
+        public string? nombreUsuario { get; set; }
+        public string? clave  { get; set; }
+        public string? nombre { get; set; }
+        public string? apellido { get; set; }
+        
+
+    }
+}
---
+++ L0_NUMEROS_CARNETS/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using L0_NUMEROS_CARNETS.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UsuariosDBContext>(options =>
+             options.UseSqlServer(
+             builder.Configuration.GetConnectionString("blogDBconnection")
+             )
+             );
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
