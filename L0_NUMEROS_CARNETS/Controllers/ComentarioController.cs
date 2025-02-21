d6f1b6a63c99b4d3b4812203ecb26595e83a8622
Roderico Guinea <guinearoderico2@gmail.com>
2025-02-20 19:07:40 -0600

    Creacion de Controlador de comentarios

---
+++ L0_NUMEROS_CARNETS/Controllers/ComentarioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using L0_NUMEROS_CARNETS.Models;
+
+namespace L01_NUMEROS_CARNETS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly BlogContext _context;
+
+        public ComentariosController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comentario>>> GetComentarios()
+        {
+            return await _context.Comentarios.ToListAsync();
+        }
+    }
+}
